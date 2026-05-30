e78f22c2532c9c764d55a803dc9a735f28c70728
KOMALPATEL <129144783+KOMALPATE@users.noreply.github.com>
2026-05-30 14:59:11 +0530

    tour in table design and binding

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponentComponent } from '../auth/login.component/login.component.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ToursComponent } from './dashboard/tours/tours.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
@@ -11,6 +12,7 @@ export const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
+    canActivate: [authGuard],
   },
   {
     path: 'tours',
